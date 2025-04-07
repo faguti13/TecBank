55b85c18b96472d4a80093906f7ab1ff75fd72ba
AlexisAlvaradoB <luialvarado@estudiantec.cr>
2025-04-06 22:18:48 -0600

    Columnas Datos
    Columnas de ingreso de datos

--- adminApp/src/components/Clientes.tsx
+++ adminApp/src/components/Clientes.tsx
@@ -73,96 +73,112 @@ function Clientes() {
             </Tabs>
           </Box>
           
-          {/** Añadir cliente */}
+          {/**************************** Añadir cliente ***********************************/}
           <div style={{display:showTab(tabActive, "1")}}>
-            <div>
-              <TextField id="standard-basic" label="Nombre" variant="standard" />
+            <div className='addClientBody'>
+              {/*Bloque de Información*/}
+              <div className='addClientBlock'>
+                <div className='addClientBlockTitle'>Información de Cuenta</div>
+                <div>
+                  <TextField id="standard-basic" label="Usuario" variant="standard" />
+                </div>
+                <div>
+                  <FormControl sx={{ m: 1, width: '25ch' }} variant="standard">
+                    <InputLabel htmlFor="standard-adornment-password">Contraseña</InputLabel>
+                    <Input
+                      id="standard-adornment-password"
+                      type={showPassword ? 'text' : 'password'}
+                      endAdornment={
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label={
+                              showPassword ? 'hide the password' : 'display the password'
+                            }
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            onMouseUp={handleMouseUpPassword}
+                            sx={{
+                              fontSize: '14px'
+                            }}
+                          >
+                            {showPassword ? "Ocultar" : "Mostrar"}
+                          </IconButton>
+                        </InputAdornment>
+                      }
+                    />
+                  </FormControl>
+                </div>
+              </div>
+              {/*Bloque de Cuenta*/}
+              <div className='addClientBlock'>
+                <div className='addClientBlockTitle'>Información de Cliente</div>
+                <div>
+                  <TextField id="standard-basic" label="Nombre" variant="standard" />
+                </div>
+                <div>
+                  <TextField id="standard-basic" label="Apellido" variant="standard" />
+                </div>
+                <div>
+                  <FormControl>
+                    <FormLabel id="radio-group-label">Tipo de Cliente</FormLabel>
+                    <RadioGroup
+                      row
+                      aria-labelledby="demo-row-radio-buttons-group-label"
+                      name="row-radio-buttons-group"
+                    >
+                      <FormControlLabel value="Físico" control={<Radio />} label="Físico" />
+                      <FormControlLabel value="Jurídico" control={<Radio />} label="Jurídico" />
+                    </RadioGroup>
+                  </FormControl>
+                </div>
+                <div>
+                  <TextField
+                    id="standard-number"
+                    label="Cédula"
+                    type="number"
+                    variant="standard"
+                    slotProps={{
+                      inputLabel: {
+                        shrink: true,
+                      },
+                    }}
+                  />
+                </div>
+              </div>
+              {/*Bloque de Identificación*/}
+              <div className='addClientBlock'>
+                <div className='addClientBlockTitle'>Otros Datos</div>
+                <div>
+                  <TextField id="standard-basic" label="Teléfono" variant="standard" />
+                </div>
+                <div>
+                  <FormControl fullWidth sx={{maxWidth: '200px' }} variant="standard">
+                    <InputLabel htmlFor="standard-adornment-amount">Ingreso Mensual</InputLabel>
+                    <Input
+                      id="standard-adornment-amount"
+                      startAdornment={<InputAdornment position="start">₡</InputAdornment>}
+                    />
+                  </FormControl>
+                </div>
+                <div>
+                  <TextField
+                    id="standard-multiline-static"
+                    label="Dirección/Domicilio"
+                    multiline
+                    rows={3}
+                    variant="standard"
+                  />
+                </div>
+              </div>
+              
             </div>
-            <div>
-              <TextField id="standard-basic" label="Apellido" variant="standard" />
-            </div>
-            <div>
-              <TextField
-                id="standard-number"
-                label="Cédula"
-                type="number"
-                variant="standard"
-                slotProps={{
-                  inputLabel: {
-                    shrink: true,
-                  },
-                }}
-              />
-            </div>
-            <div>
-            <TextField
-              id="standard-multiline-static"
-              label="Dirección/Domicilio"
-              multiline
-              rows={3}
-              variant="standard"
-            />
-            </div>
-            <div>
-            <TextField id="standard-basic" label="Teléfono" variant="standard" />
-            </div>
-            <div>
-            <FormControl fullWidth sx={{maxWidth: '200px' }} variant="standard">
-              <InputLabel htmlFor="standard-adornment-amount">Ingreso Mensual</InputLabel>
-              <Input
-                id="standard-adornment-amount"
-                startAdornment={<InputAdornment position="start">₡</InputAdornment>}
-              />
-            </FormControl>
-            </div>
-            <div>
-              <FormControl>
-                <FormLabel id="radio-group-label">Tipo de Cliente</FormLabel>
-                <RadioGroup
-                  row
-                  aria-labelledby="demo-row-radio-buttons-group-label"
-                  name="row-radio-buttons-group"
-                >
-                  <FormControlLabel value="Físico" control={<Radio />} label="Físico" />
-                  <FormControlLabel value="Jurídico" control={<Radio />} label="Jurídico" />
-                </RadioGroup>
-              </FormControl>
-            </div>
-            <div>
-              <TextField id="standard-basic" label="Usuario" variant="standard" />
-            </div>
-            <div>
-              <FormControl sx={{ m: 1, width: '25ch' }} variant="standard">
-                <InputLabel htmlFor="standard-adornment-password">Contraseña</InputLabel>
-                <Input
-                  id="standard-adornment-password"
-                  type={showPassword ? 'text' : 'password'}
-                  endAdornment={
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label={
-                          showPassword ? 'hide the password' : 'display the password'
-                        }
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                        onMouseUp={handleMouseUpPassword}
-                        sx={{
-                          fontSize: '14px'
-                        }}
-                      >
-                        {showPassword ? "Ocultar" : "Mostrar"}
-                      </IconButton>
-                    </InputAdornment>
-                  }
-                />
-              </FormControl>
-            </div>
+            
           </div>
-          {/** Modificar cliente */}
+          {/**************************** Modificar cliente ***********************************/}
           <div style={{display:showTab(tabActive, "2")}}>
             Modificar Cliente
           </div>
-          {/** Eliminar cliente */}
+          {/**************************** Eliminar cliente ***********************************/}
           <div style={{display:showTab(tabActive, "3")}}>
             Eliminar Cliente
           </div>
