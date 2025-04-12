5979cce1595f5817744bc5cac945ccbd3d6e7fb3
unknown <fagutierrez@estudiantec.cr>
2025-04-12 14:55:00 -0600

    gitignore debug

--- api/TecBankAPI/obj/Debug/net9.0/TecBankAPI.AssemblyInfo.cs
+++ api/TecBankAPI/obj/Debug/net9.0/TecBankAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TecBankAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+390d24fd5dfbd063a18ec8e5b4be6788aaa5bd7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce390caf5971bc5da593e9cfaa8650cdd6a62bec")]
 [assembly: System.Reflection.AssemblyProductAttribute("TecBankAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TecBankAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
