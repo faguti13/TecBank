4e9177f42aa3c0ed1d208ffaa80f0c28857c6d5e
Emmanuel Calvo <emmanuelcalvomora984@gmail.com>
2025-04-13 07:59:29 -0600

    ajustes minimos

--- api/TecBankAPI/Program.cs
+++ api/TecBankAPI/Program.cs
@@ -3,7 +3,6 @@ using TecBankAPI.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
