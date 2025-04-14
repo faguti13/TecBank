9a881b3f04ed638cc7fe10c44b9f970b7543f28a
unknown <fagutierrez@estudiantec.cr>
2025-04-13 18:52:23 -0600

    merge refactorAdmin

--- api/TecBankAPI/obj/Debug/net9.0/TecBankAPI.AssemblyInfo.cs
+++ api/TecBankAPI/obj/Debug/net9.0/TecBankAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TecBankAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42a77bff19984ebe95f2b2b5a0146966ca6c9e06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66b18096115826136ce6c44fdc2b1780ea8b2081")]
 [assembly: System.Reflection.AssemblyProductAttribute("TecBankAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TecBankAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
