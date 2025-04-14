77beded116fd17fc917530bcd597df06c3ddbb67
unknown <fagutierrez@estudiantec.cr>
2025-04-13 20:24:03 -0600

    merge cuentasClienteApp

--- api/TecBankAPI/obj/Debug/net9.0/TecBankAPI.AssemblyInfo.cs
+++ api/TecBankAPI/obj/Debug/net9.0/TecBankAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TecBankAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d146ced99bb3ae1cfea370c580c316354e2a2cf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2ac5f5642c5021b793072182ca2b36ddf261e5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TecBankAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TecBankAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
